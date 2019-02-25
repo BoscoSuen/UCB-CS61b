c5d7fded6da2078aeb30aacf675a427f854b66eb
Yukinichi <boscosuen@outlook.com>
2019-02-25 19:12:12 +0800

    homework1 completed

--- homeworks/src/hw1/Nuke2.java
+++ homeworks/src/hw1/Nuke2.java
@@ -1,9 +1,27 @@
 package hw1;
 
+import java.net.*;
+import java.io.*;
+
 public class Nuke2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		BufferedReader keyboard;
+		String str;
+		
+		keyboard = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("type a string from your keyboard:");
+	    System.out.flush();        /* Make sure the line is printed immediately. */
+	    
+	    str = keyboard.readLine();   //when encounter error, add "throw Exception at the method declaration"
+	    
+	    StringBuffer strbuff = new StringBuffer(str);
+	    strbuff.delete(1,2);      //remove the second character
+
+	    System.out.println("Now remove the second character:");
+	    System.out.println(strbuff);
+	    
 
 	}
 
